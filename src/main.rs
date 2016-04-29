972af3a6417e223afaa326fe00b735cd996fc3ec
Sehny <sehnoutka.martin@gmail.com>
2016-04-29 11:52:27 +0200

    function declarations, unit tests

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,38 @@ const MEMORY_SIZE: usize = 30000;
 
 impl Interpreter {
     /// Create new interpreter with empty program and default memory size.
-    fn new() -> Interpreter {
+    pub fn new() -> Interpreter {
         Interpreter {
             ip: 0,
             dp: 0,
             mem: Box::new([0u8; MEMORY_SIZE]),
             prog: Vec::new(),
         }
+    }
+
+    /// Read tape with program (where 'tape' is represented by string)
+    /// Return number of valid brainfuck characters
+    pub fn read_tape(&self, input: String) -> usize {
+        for c in input.chars() {
+            //println!("{}", c);
+            match c {
+                '+' | '-' | '>' | '<' | '[' | ']' | '.' | ',' => {
+                    //println!("bf code");
+                },
+                _ => {
+                    //println!("non bf");
+                }
+            }
+        }
+        0
+    }
+
+    /// Read next bf instruction
+    fn read_next_instruction(&self) {
+    }
+
+    /// Run brainfuck program
+    pub fn run(&self) {
     }
 }
 
@@ -33,13 +58,55 @@ fn main() {
     let mut a = Interpreter::new();
     a.mem[0] = 1;
     assert!(a.mem[0] != 0);
+    a.read_tape("Ahoj +++>++>+".to_string());
 }
 
 #[cfg(test)]
-mod test {
-    #[test]
-    #[should_panic]
-    fn test_sth() {
-        panic!("Test");
-    }
+#[test]
+#[should_panic]
+fn test_sth() {
+    panic!("Test");
+}
+
+#[cfg(test)]
+#[test]
+fn test_tape_reading() {
+    let bfi = Interpreter::new();
+    bfi.read_tape("abcd*()@+-[]<>.,".to_string());
+    assert!(bfi.prog == b"+-[]<>.,");
+}
+
+#[cfg(test)]
+#[test]
+fn test_read_instruction() {
+    let bfi = Interpreter::new();
+    bfi.read_tape("+".to_string());
+    bfi.read_next_instruction();
+    assert!(bfi.mem[0] == 1); 
+
+    let bfi = Interpreter::new();
+    bfi.read_tape("++-".to_string());
+    bfi.read_next_instruction();
+    assert!(bfi.mem[0] == 1); 
+    
+    let bfi = Interpreter::new();
+    bfi.read_tape(">+".to_string());
+    bfi.read_next_instruction();
+    assert!(bfi.mem[1] == 1); 
+
+    let bfi = Interpreter::new();
+    bfi.read_tape(">><+".to_string());
+    bfi.read_next_instruction();
+    assert!(bfi.mem[1] == 1); 
+
+    let bfi = Interpreter::new();
+    bfi.read_tape("[+++>>>]+++++".to_string());
+    bfi.read_next_instruction();
+    assert!(bfi.ip == 8); 
+
+    let mut bfi = Interpreter::new();
+    bfi.read_tape("++[+-><]+".to_string());
+    bfi.ip = 7;
+    bfi.read_next_instruction();
+    assert!(bfi.ip == 3); 
 }
